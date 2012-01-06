b0be485e39308df658ea5b21d5f7be52ccb6c738
Edgar Marca <matiskay@gmail.com>
2012-01-05 23:42:33 -0500

    Scrape to pages

--- icone/spiders/icone_spider.py
+++ icone/spiders/icone_spider.py
@@ -1,10 +1,9 @@
 import re
+from urlparse import urlparse
 
-from scrapy import log
 from scrapy.spider import BaseSpider
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.http import Request      
 
 from icone.items import Product 
@@ -22,28 +21,63 @@ def clear(l):
     return ' '.join(r_list)
 
 
+# Generate a slug string
+# This is not a real implemention of slug but it works in this case
+def slug(s):
+  slug = s.replace(' ', '-')
+  return slug
+
+
+# Remove the last element of a url
+# TODO: Refactor this code
+def remove_last(url):
+    url = urlparse(url)
+
+    path = '/'.join(url.path.split('/')[1:len(url.path.split('/')) - 2 ])
+
+    url = 'http://%s/%s/' % (url.netloc, path)
+
+    return url
+
+
 class IconeSpider(BaseSpider):
 
     name = 'icone'
     allowed_domains = ['icone.co.uk']
-    start_urls = ['http://www.icone.co.uk/designer-living/product-type/Lighting-Floor',
+    start_urls = ['http://www.icone.co.uk',
 #        'http://www.icone.co.uk/designer-living/product-type/',
 #        'http://www.icone.co.uk/designer-living/brand/',
 #        'http://www.icone.co.uk/designer-living/designer/',
     ]
 
-    rules = [Rule(SgmlLinkExtractor(allow=('/\d+',)), callback='parse', follow=True), ]
 
     def parse(self, response):
-        log.msg('A response from %s just arrived!' % response.url)
+        hxs = HtmlXPathSelector(response)
+
+        # Remove the first element becuase it doesn't provide any item
+        product_types = hxs.select('//select[@name="prod_type"]/option/text()').extract()[1:] 
+
+        for product_type in product_types:
+          product_type = slug(product_type)
+          # TODO: There is a problem adding 0
+          url = "%s/designer-living/product-type/%s/0/" % (response.url, product_type)
+          yield Request(url=url, callback=self.parse_pages)
+
+
+    def parse_pages(self, response):
 
         hxs = HtmlXPathSelector(response)
 
         pages = len(hxs.select('//table/tr/td/table/tr[3]/td/table/tr[2]/td/form/table/tr[3]/td/table/tr/td/a'))
 
+        if pages == 0:
+            pages = 1
+
         for number in range(0, pages):
-            url = "%s/%s/" % (response.url, number)
-            yield Request(url=url, callback=self.parse_products)
+            url = "%s%s/" % (remove_last(response.url), number)
+            # dont_filter=True beacuse the spider doesn't read the first request /0/
+            yield Request(url=url, callback=self.parse_products, dont_filter=True)
+
 
     def parse_products(self, response):
         hxs = HtmlXPathSelector(response)
